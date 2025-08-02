b623918bfb1bb3bf083fa9170413661a046bc5e4
hustlepn <pranavnarayanan0007@gmail.com>
2025-08-02 10:19:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/downloader')
+def downloader():
+    return render_template('tools/downloader.html')
+
+@app.route('/urlshortener')
+def url_shortener():
+    return render_template('tools/urlshortener.html')
+
+@app.route('/calculators')
+def calculators():
+    return render_template('tools/calculators.html')
+
+@app.route('/pdf')
+def pdf_tools():
+    return render_template('tools/pdftools.html')
+
+@app.route('/qrcode')
+def qr_generator():
+    return render_template('tools/qrcode.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
